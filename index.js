ccfb05cd74c5c5c7d87bae826eac395a62c8e291
JulienKisoni <kisonijulien@gmail.com>
2020-03-25 17:08:27 +0100

    Connection To DB ok

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
 const mongoose = require('mongoose');
 const cors = require('cors');
-const socket = require('socket.io');
-require('dotenv').config();
+const socket = require('socket.io')(http);
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(cors);
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB'))
+    .catch(e => console.error('Not connected to DB', e));
 
 app.listen('4000', () => {
-    console.log('app listening on port 4000');
+    console.log('App listening on Port 4000');
 });
