08c2d6a2c7294fb657cc11d773966165de244190
JulienKisoni <kisonijulien@gmail.com>
2020-07-08 14:17:46 +0100

    ready

--- mongoose/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    username: { type: String },
-    email: { type: String },
-    picture: { type: String }
-});
-
-module.exports = mongoose.model('Users', UserSchema);
